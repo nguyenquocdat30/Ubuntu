ebbf156e52845858f65e6b4ef0becaffff4b150c
nguyenquocdat30 <boelovenhan@gmail.com>
2017-04-21 08:16:00 +0700

    Cap Nhat Lai File cong.sh va file tru.sh

--- tru.sh
+++ tru.sh
@@ -1,8 +1,8 @@
 clear
 echo -e "======================TINH HIEU======================\n"
-echo  "Nhap Vao So Thu Nhat : "
+echo -n "Nhap Vao So Thu Nhat : "
 read soThuNhat
-echo "Nhap Vao So Thu Hai : "
+echo -n "Nhap Vao So Thu Hai : "
 read soThuHai
 ketQua=$(($soThuNhat-$soThuHai))
 echo -e "\nKet Qua : $soThuNhat - $soThuHai = $ketQua\n"
