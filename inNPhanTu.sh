3e5a037d54541c6c863dbb3897ffb5b588fd0a6e
nguyenquocdat30 <boelovenhan@gmail.com>
2017-04-21 08:23:23 +0700

    Cap Nhat Tinh Tong Cac Phan Tu Trong Mang

--- inNPhanTu.sh
+++ inNPhanTu.sh
@@ -7,9 +7,12 @@ do
 echo -n "Mang[$i] =  "
 read Mang[i]
 done
+tong=0
 echo -e "Danh sach cac phan tu : \n"
 for((i=0;i<soPhanTu;i++))
 do
+tong=$(($tong+${Mang[i]}))
 echo -n "${Mang[i]} "
 done
-echo -e "\n"
+echo -e "\n ====================================================="
+echo "Tong cac phan tu trong mang la : $tong"
