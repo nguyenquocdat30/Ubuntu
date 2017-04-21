d3fcca9de3f4cc46c36471d63c8fe804abe7ca98
nguyenquocdat30 <boelovenhan@gmail.com>
2017-04-21 07:44:24 +0700

    lan 1

---
+++ tru.sh
@@ -0,0 +1,9 @@
+clear
+echo -e "======================TINH HIEU======================\n"
+echo  "Nhap Vao So Thu Nhat : "
+read soThuNhat
+echo "Nhap Vao So Thu Hai : "
+read soThuHai
+ketQua=$(($soThuNhat-$soThuHai))
+echo -e "\nKet Qua : $soThuNhat - $soThuHai = $ketQua\n"
+
